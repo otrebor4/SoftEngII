8d036e5d708ff55e859576313dce6959c0974f52
raramos21 <araulramos@gmail.com>
2015-04-27 19:33:34 -0500

    Added tables and info

--- templates/help.php
+++ templates/help.php
@@ -1,1 +1,44 @@
-help
+<div class = 'main_body'>
+    <style type="text/css">
+        .table {border-collapse:collapse;border-spacing:0;border-color:#bbb;margin:0px auto; width: 100%}
+        .table td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;
+                border-width:0px;overflow:hidden;word-break:normal;border-color:#bbb;color:#594F4F;background-color:#E0FFEB;}
+        .table th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;
+                border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#bbb;color:#493F3F;background-color:#9DE0AD;}
+        .table .thead {font-size:32px;font-family:"Arial Black", Gadget, sans-serif;background-color:#A37547;text-align:center}
+        .table .tcells {font-size:18px;background-color:#ffffff; border-style: dashed;border-width: 1px; padding-right: 20px; padding-left: 20px}
+        .table .tcells2 {font-size:20px;background-color:#ffffff;text-align: center; padding: 30px}
+    </style>
+
+
+    <table class="table">
+        <tr>
+            <th class="thead" colspan="3">Help</th>
+        </tr>
+        <tr>
+            <td class="tcells2" colspan="3">Buy and Selling CryptoCoins </td>
+        </tr>
+        <tr>
+            <td class="tcells" colspan="3">Cryptocurrency is a rapidly growing alternative to paying with credit or prepaid cards.
+                We believe within the next 10 years, major online retailers will offer their customers the option to pay using a
+                variety of cryptocurrency coins. Crypto-Wallet will allow the user to track currency, transactions, and deposits,
+                and exchange cryptocurrency with others across the world.</td>
+        </tr>
+        <tr>
+            <td class="tcells2" colspan="3">Buy and Selling Crypto-Coins </td>
+        </tr>
+        <tr>
+            <td class="tcells"></td>
+            <td class="tcells"></td>
+            <td class="tcells"></td>
+        </tr>
+        <tr>
+            <td class="tcells2" colspan="3">How to Trade</td>
+        </tr>
+        <tr>
+            <td class="tcells"></td>
+            <td class="tcells"></td>
+            <td class="tcells"></td>
+        </tr>
+    </table>
+</div>
